2347a45d1a38419b976b8f63366dcca6c1b3a18b
Fedor002 <anakek228@mail.ru>
2023-03-07 19:17:40 +0300

    Viola_Jons

--- T3.py
+++ T3.py
@@ -1,12 +1,12 @@
 import cv2
 
 #Считывание фотографии
-img_rgb = cv2.imread('Stock/Stock5.jpg')
+img_rgb = cv2.imread('Stock/StockC2.jpg')
 #Перекидываю код метода в объект
 face_casd = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # Обнаружение лиц с помощью детектора Виолы-Джонса
-faces = face_casd.detectMultiScale(img_rgb, scaleFactor=1.5,minNeighbors=5, minSize=(20,20))
+faces = face_casd.detectMultiScale(img_rgb, scaleFactor=1.5,minNeighbors=1, minSize=(40,40))
 for (x,y,w,h) in faces:
     #Обводка по координатам, после использования метода
     cv2.rectangle(img_rgb, (x,y),(x+w, y+h),(0, 0, 0),2)
